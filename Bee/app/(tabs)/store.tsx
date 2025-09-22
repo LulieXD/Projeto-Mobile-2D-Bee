65dd2bb7960b4ba3c51ef4dc24b925849c91399b
Flaviobeep <flavio.santos.gomes@escola.pr.gov.br>
2025-09-22 12:27:27 -0300

    Página store limpa para adicionar os elementos

--- Bee/app/(tabs)/store.tsx
+++ Bee/app/(tabs)/store.tsx
@@ -1,82 +1,25 @@
 /*loja de jogos ou carrinho*/
-import { Image } from 'expo-image';
-import { Platform } from 'react-native';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet } from 'react-native';
 
-export default function TabTwoScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
 
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Bee Games</ThemedText>
-      </ThemedView>
-      <ThemedText></ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          {' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        
-      
-      <Collapsible title="Light and dark mode components">
-        
-        
-      </Collapsible>
-      <Collapsible title="Animations">
-        
-        {Platform.select({
-          ios: (
-            
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+export default function HomeScreen() {
+  return;
+}
 
 const styles = StyleSheet.create({
-  headerImage: {
-    bottom: 90,
-    left: 35,
-    position: 'absolute',
-  },
   titleContainer: {
     flexDirection: 'row',
+    alignItems: 'center',
     gap: 8,
   },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
   reactLogo: {
-    height: 300,
-    width: 800,
+    height: 178,
+    width: 290,
     bottom: 0,
     left: 0,
     position: 'absolute',
